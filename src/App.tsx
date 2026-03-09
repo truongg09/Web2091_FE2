571abef0f210a398096fe7438dd17755cdc625a0
truong09 <truongconn09@gmail.com>
2026-03-09 19:28:35 +0700

    lab1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
+import { Button, Layout, Input, Form } from "antd";
+import { Route, Routes } from "react-router-dom";
+import Lab1 from "./pages/Lab1";
 
+// const { Header, Content, Footer } = Layout;
 function App() {
+  // const onFinish = (values: any) => {
+  //   console.log(values);
+  // };
   return (
     <>
       <nav className="bg-blue-600 text-white shadow">
@@ -11,7 +18,7 @@ function App() {
           </Link>
 
           <div className="hidden md:flex items-center space-x-8">
-            <Link to="#" className="hover:text-gray-200">
+            <Link to="/" className="hover:text-gray-200">
               Trang chủ
             </Link>
             <Link to="/list" className="hover:text-gray-200">
@@ -35,7 +42,48 @@ function App() {
 
       {/* MAIN CONTENT */}
       <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
-        <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
+        <div className="mb-2">
+          <Link to="/lab1">
+            <Button className="mr-1" type="primary">Lab1</Button>
+          </Link>
+
+          <Link to="/lab2">
+            <Button className="mr-1" type="default">Lab2</Button>
+          </Link>
+
+          <Link to="/lab3">
+            <Button className="mr-1" type="dashed">Lab3</Button>
+          </Link>
+
+          <Link to="/lab4">
+            <Button className="mr-1" type="link">Lab4</Button>
+          </Link>
+
+          <Link to="/lab5">
+            <Button type="text">Lab5</Button>
+          </Link>
+        </div>
+
+        {/* <Layout className="mt-2">
+          <Header style={{ color: "white" }}>Header</Header>
+          <Content style={{ padding: 20 }}>
+          <Form onFinish={onFinish}>
+              <Form.Item label="Username" name="username">
+                <Input />
+              </Form.Item>
+              <Form.Item>
+                <Button htmlType="submit" type="primary">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Content>
+          <Footer>Footer</Footer>
+        </Layout> */}
+
+        <Routes>
+          <Route path="/lab1" element={<Lab1/>}></Route>
+        </Routes>
       </div>
 
       <Toaster />
---
+++ src/pages/Lab1.tsx
@@ -0,0 +1,101 @@
+import { Layout, Menu, Table, Button, Modal, Form, Input } from "antd";
+import { useState } from "react";
+
+const { Header, Sider, Content } = Layout;
+
+function Lab1() {
+
+  const [open, setOpen] = useState(false);
+  const [users, setUsers] = useState([
+    { key: 1, name: "Ngô Quang Trường", email: "truong09@gmail.com", role: "Admin" },
+  ]);
+
+  const [form] = Form.useForm();
+
+  const columns = [
+    { title: "Name", dataIndex: "name" },
+    { title: "Email", dataIndex: "email" },
+    { title: "Role", dataIndex: "role" },
+  ];
+
+  const onFinish = (values:any) => {
+    const newUser = {
+      key: users.length + 1,
+      ...values
+    };
+
+    setUsers([...users, newUser]);
+    setOpen(false);
+    form.resetFields();
+  };
+
+  return (
+    <Layout style={{ minHeight: "70vh" }}>
+
+      {/* Sidebar */}
+      <Sider>
+        <div className="mt-4">
+            <Menu
+            theme="dark"
+            mode="inline"
+            items={[
+                { key: 1, label: "Trang chủ" },
+                { key: 2, label: "Users" },
+            ]}
+            />
+        </div>
+      </Sider>
+
+      <Layout>
+
+        {/* Header */}
+        <Header style={{ color: "white", fontSize: 20, background: "#1677ff"  }}>
+          Quản lý tài khoản
+        </Header>
+
+        {/* Content */}
+        <Content style={{ padding: 20}}>
+
+            <div className="flex justify-end mb-5">
+                <Button type="primary" onClick={() => setOpen(true)}>
+                Add User
+                </Button>
+            </div>
+
+          <Table columns={columns} dataSource={users} />
+
+          {/* Modal */}
+          <Modal
+            title="Add User"
+            open={open}
+            footer={null}
+            onCancel={() => setOpen(false)}
+          >
+            <Form form={form} layout="vertical" onFinish={onFinish}>
+
+              <Form.Item label="Name" name="name">
+                <Input />
+              </Form.Item>
+
+              <Form.Item label="Email" name="email">
+                <Input />
+              </Form.Item>
+
+              <Form.Item label="Role" name="role">
+                <Input />
+              </Form.Item>
+
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+
+            </Form>
+          </Modal>
+
+        </Content>
+      </Layout>
+    </Layout>
+  );
+}
+
+export default Lab1;
